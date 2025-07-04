0f2bd669f56cace363a17f8660e8bc5960202316
BATMAN <BATMAN@Rickys-MacBook-Air.local>
2025-07-04 17:37:07 +0530

    Added Vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import MoviesPage from './pages/MoviesPage';
 import TVShowsPage from './pages/TVShowsPage';
 import SpecialPresentationsPage from './pages/SpecialPresentationsPage';
 import NotFound from './pages/NotFound';
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
@@ -33,6 +34,7 @@ const App = () => {
           </BrowserRouter>
         </AuthProvider>
       </TooltipProvider>
+      <Analytics />
     </QueryClientProvider>
   );
 };
